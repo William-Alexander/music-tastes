f4396ab38634e0ecc94ad160256194e8e633faf4
Will Alexander <freegravity3@gmail.com>
2017-03-09 18:39:28 -0600

    Separated Needledrop scraping into another file.
    Hello again! It's been a while since I've put anything into this repo, but it's time for me to finish this lil project up.

---
+++ needledrop.py
@@ -0,0 +1,55 @@
+'''
+Gets information from the Needle Drop website, puts it into one file
+with each line of format "artist - album - score"
+'''
+
+from bs4 import BeautifulSoup
+import unidecode
+import requests
+import time 
+
+site_prefix = "http://www.theneedledrop.com"
+all_entries = []
+file_name = "needledrop.txt"
+f = open(file_name, 'w')
+headers = {'user-agent': 'Your friend, Will.'}
+
+for i in xrange(0, 11):
+	def init_start_url(rating):
+		return ("http://www.theneedledrop.com/articles/?tag=" + 
+				str(rating) + "%2F10")
+
+	start_url = init_start_url(i)
+
+	url = start_url
+	last_page = False
+
+	while not last_page:
+		r = requests.get(url, headers=headers)
+		soup = BeautifulSoup(r.text, "html.parser")
+
+		# Check to see if we've been sending too many requests
+		# If so, just wait a second and try again! :)
+		if soup.title.string == "429 Too Many Requests":
+			time.sleep(1)
+			continue
+
+		page_entries = soup.find_all("h1", class_="entry-title")
+
+		for album in page_entries:
+			# This time, we're keeping the artist name. Change non-ascii
+			# characters into their ascii counterparts, tho.
+			txt_entry = str(unidecode.unidecode(album.a.get_text()))
+			txt_entry += " - " + str(i)
+			print txt_entry
+			f.write((txt_entry.encode('ascii', 'ignore')) + "\n")
+
+		older = soup.find(class_="older")
+
+		if older is not None and older.string:
+			url = site_prefix + older.a["href"]
+			time.sleep(0.5) 
+		else:
+			last_page = True
+
+f.close()
