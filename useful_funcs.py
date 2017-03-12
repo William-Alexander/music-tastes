d9ab090cf3af9112f67016a24291f5567dea2365
Will Alexander <freegravity3@gmail.com>
2017-03-12 14:21:44 -0500

    Oh yeah, this one too.

--- useful_funcs.py
+++ useful_funcs.py
@@ -1,3 +1,5 @@
+import re
+
 '''
 So the idea is that if we take out all of the characters except for the 
 alphabet, numbers, and spaces, we can compare things more easily since
@@ -7,12 +9,6 @@ punctuation.
 def strip_chars(my_str):
 	"""
 	Removes all annoying characters and articles, ya dig?
-
-	w.r.t remove_articles: every rule requires a precident, right? Pitchfork
-	and Needle Drop differ sometimes in how they use articles, so I just get
-	rid of 'em all.
-
-	IT'S NOT PERFECT unfortunately but whatever it's conservative.
 	"""
 	my_str = my_str.lower()
 	def remove_articles(my_str):
@@ -21,3 +17,31 @@ def strip_chars(my_str):
 	my_str = remove_articles(my_str)
 	regex = re.compile('[^a-z0-9\'\- ]')
 	return regex.sub('', my_str)
+
+def get_albums(my_file="needledrop.txt"):
+	"""
+	Nabs all the artists and corresponding albums from the masterfile
+	that contains all of the albums the Needle Drop has reviewed. To
+	preserve order, I use a list.
+
+	Does not include review scores.
+	"""
+	with open(my_file) as f:
+		content = f.readlines()
+		content = [x.strip("\n") for x in content]
+
+	album_list = []
+
+	for item in content:
+		# Python 3 supports extended iterable unpacking, but not 2 :(
+		artist, album, _, _ = item.split(" ::: ")
+		album_list.append((artist, album))
+
+	return album_list
+
+def fix_album(album):
+	"""
+	Just makes the album nice and presentable to the sites.
+	"""
+	album = strip_chars(album)
+	return album.replace(" ", "%20")
